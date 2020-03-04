f0611f2eccdbc099381be0325eccb0f6f2d2ff10
Katapangan55 <jjm121197@gmail.com>
2020-03-04 01:07:39 -0600

    Initial

---
+++ WebServer.java
@@ -0,0 +1,27 @@
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
+
+
+public final class WebServer
+{
+  public static void main(String argv[]) throws Exception
+  {
+  // Set the port number.
+  int port = 6789;
+  //. . .
+  }
+
+}
+
+
+/*
+final class HttpRequest implements Runnable
+{
+
+}
+
+public static void main(String argv[]) throws Exception
+{
+  int port = 6789;
+}*/
