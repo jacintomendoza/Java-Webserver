6649a7a6cef8179708fe638b4fc2951c3f2a087f
Katapangan55 <jjm121197@gmail.com>
2020-03-06 10:03:59 -0600

    Added loop

--- WebServer.java
+++ WebServer.java
@@ -1,18 +1,30 @@
 import java.io.* ;
 import java.net.* ;
 import java.util.* ;
+import java.net.ServerSocket;
 
 
 public final class WebServer
 {
   public static void main(String argv[]) throws Exception
   {
-  // Set the port number.
-  int port = 6789;
-  //. . .
+    int port = 6789;
+
+    final ServerSocket server = new ServerSocket(port);
+    System.out.println("Listening for port " + port);
+
+    // Process HTTP service requests in an infinite loop
+    while (true)
+    {
+      // Listen for a TCP connection request
+    }
+
+
   }
 
 }
+
+// Establish the listen socket
 
 
 /*
@@ -20,8 +32,4 @@ final class HttpRequest implements Runnable
 {
 
 }
-
-public static void main(String argv[]) throws Exception
-{
-  int port = 6789;
-}*/
+*/
